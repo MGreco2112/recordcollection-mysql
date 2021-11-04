11b842f47130b43944e606d0de230be9b10c90d0
Matthew Greco <mgreco2112@tutanota.com>
2021-11-04 14:54:57 -0400

    created basic project

---
+++ src/test/java/com/recordcollection/collection/CollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recordcollection.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
