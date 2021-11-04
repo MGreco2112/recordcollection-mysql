6fb967a5b4ae71580a5fe495e3b3681b2e57eb5f
Matthew Greco <mgreco2112@tutanota.com>
2021-11-04 18:43:47 -0400

    added Repository annotations

--- src/main/java/com/recordcollection/collection/repositories/AlbumRepository.java
+++ src/main/java/com/recordcollection/collection/repositories/AlbumRepository.java
@@ -2,6 +2,8 @@ package com.recordcollection.collection.repositories;
 
 import com.recordcollection.collection.models.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
 }
